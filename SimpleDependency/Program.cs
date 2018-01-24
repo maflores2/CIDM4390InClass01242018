6af396d0f9361b6205dfca4af3e38c33347bd00b
Max Flores <mf947098@wtstudents.wtamu.edu>
2018-01-24 15:57:05 -0600

    Initial Commit

---
+++ SimpleDependency/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using MessagePrinter;
+
+namespace SimpleDependency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var service = new MessagePrintingService();
+            service.PrintMessage();
+            Console.WriteLine("Press a key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
